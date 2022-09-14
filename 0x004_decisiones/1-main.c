80b6fe54236abba9205bffb3f58e0c63b5109f1e
huanjo92 <jjcanche.palmas@gmail.com>
2022-09-14 13:26:11 -0500

    prueba push

---
+++ 0x004_decisiones/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{ 
+    int age;
+
+    printf("Enter your age:\n");
+    scanf("%d",&age);
+
+    if(age >= 18)
+    {
+        printf("Puedes votar \n");
+    }
+    else
+    {
+        printf("No puede votar \n");
+    }
+    return 0;
+}
