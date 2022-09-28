9d295e9a36e0205dca4c1d03ec9ce600278fb9d8
Juanho92 <112290757+Juanho92@users.noreply.github.com>
2022-09-27 21:36:44 -0500

    add via push
    tarea 3

---
+++ 0x01_Funciones/5-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+     int resultado = 0;
+
+    resultado = div(6,5);
+    printf("El resultado de la division es: %d\n",resultado);
+    resultado = div(3,5);
+    printf("El resultado de la division es: %d\n",resultado);
+    resultado = div(4,4);
+    printf("el resultado de la division es: %d\n", resultado);
+}
